a44879a2646a95a7630c96b0b9e6ded32d6bd889
grro <gregror.roth@1und1.de>
2025-12-31 11:25:51 +0100

    web server added

--- media_web.py
+++ media_web.py
@@ -9,9 +9,9 @@ from typing import List, Dict, Any
 
 class SimpleRequestHandler(BaseHTTPRequestHandler):
 
-    def log_message(self, format, *args):
-        # suppress access logging
-        pass
+    #def log_message(self, format, *args):
+    #    # suppress access logging
+    #    pass
 
     def do_GET(self):
         # Access the media instance shared via the server object
